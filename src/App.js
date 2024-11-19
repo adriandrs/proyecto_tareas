fa3e5b812241f81c01219c4ec0e4bfcffb7250f4
Adrian Santelis <adrs170303@gmail.com>
2024-11-18 16:02:31 -0800

    Avance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const tareasT = [
+  {
+    nombre: "Tarea 1",
+    descripcion: "Descripcion 1",
+    estatus: "Hecha",
+  },
+  {
+    nombre: "Tarea 2",
+    descripcion: "Descripcion 2",
+    estatus: "Pendiente",
+  },
+  {
+    nombre: "Tarea 3",
+    descripcion: "Descripcion 3",
+    estatus: "Retrasada",
+  },
+];
 
 function App() {
+  const [tareas, setTareas] = useState(
+    tareasT || localStorage.getItem("tareas")
+  );
+  const [tareasFiltradas, setTareasFiltradas] = useState([]);
+
+  useEffect(() => {
+    setTareasFiltradas(tareas);
+    localStorage.setItem("tareas", tareas);
+  }, []);
+
+  const estatusClassNames = (estatus) => {
+    return {
+      width: "fit-content",
+      backgroundColor: estatus === "Hecha" ? "#d4edda" : "#f8d7da",
+      color: estatus === "Hecha" ? "#155724" : "#721c24",
+      padding: "10px",
+      border: "1px solid #ddd",
+      marginBottom: "10px",
+    };
+  };
+
+  const agregarTarea = () => {};
+  const modificar = () => {};
+  const eliminarTarea = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Projecto Tareas</h1>
+      <section className="app">
+        <div className="contenedor">
+          <ul className="contenedor-tareas">
+            {tareasFiltradas.map((tarea) => (
+              <li className="tarea" key={tarea.id}>
+                <p>{tarea.nombre}</p>
+                <p>{tarea.descripcion}</p>
+                <button
+                  style={estatusClassNames(tarea.estatus)}
+                  className="estatus"
+                >
+                  Estatus: {tarea.estatus}
+                </button>
+
+                <input type="file"></input>
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={() => {
+              agregarTarea();
+            }}
+          >
+            Agregar Tarea
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
 
